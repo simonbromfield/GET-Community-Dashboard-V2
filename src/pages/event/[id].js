a9191d9b05ef85cdef298ee31e1e7fb5dbec3dad
Simon Bromfield <simon@simonbromfield>
2022-10-26 00:33:38 +0100

    error fix on event profile

--- src/pages/event/[id].js
+++ src/pages/event/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import {
@@ -8,40 +8,39 @@ import {
 } from '@mui/material'
 import { DashboardLayout } from '../../components/dashboard-layout'
 
-const eventProfile = ({ wsdata }) => {
+const EventProfile = () => {
   const router = useRouter()
   const { id } = router.query
+  return (
+    <DashboardLayout>
+    <>
+    <Head>
+      <title>
+        Event Profile | GET Protocol Community
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+            <h2>EVENT PROFILE</h2>
+            <p>This page is under construction, visit this event via dashboard v1:</p>
+            <Link
+              size="large"
+              href={`https://sleepy-shore-42215.herokuapp.com/event-profile/${id}`}
+              target="_blank"
+            >
+              View Event
+            </Link>
+      </Container>
+    </Box>
+    </>
+    </DashboardLayout>
+  );
+}
 
-    return (
-      <DashboardLayout>
-      <>
-      <Head>
-        <title>
-          Event Profile | GET Protocol Community
-        </title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8
-        }}
-      >
-        <Container maxWidth={false}>
-              <h2>EVENT PROFILE</h2>
-              <p>This page is under construction, visit this event via dashboard v1:</p>
-              <Link
-                size="large"
-                href={`https://sleepy-shore-42215.herokuapp.com/event-profile/${id}`}
-                target="_blank"
-              >
-                View Event
-              </Link>
-        </Container>
-      </Box>
-      </>
-      </DashboardLayout>
-    );
-  }
-
-export default eventProfile
+export default EventProfile
