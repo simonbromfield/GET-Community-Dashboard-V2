759bbf7d423c06018b095f72f16155059764a0b2
Simon Bromfield <simon@simonbromfield>
2023-04-28 23:01:00 +0100

    styled the header, central, nav buttons each side

--- Dashboard/src/components/calendar/CalendarHeader.js
+++ Dashboard/src/components/calendar/CalendarHeader.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Box } from "@mui/material";
 
 const CalendarHeader = ({ currentMonth, handleLastMonth, handleNextMonth }) => {
   return (
-    <>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ width: "100%" }}
+    >
+      <Button
+        variant="text"
+        onClick={handleLastMonth}
+        sx={{ margin: 2, color: "green" }}
+      >
+        Previous
+      </Button>
       <Typography variant="h4" component="h2" gutterBottom>
         {currentMonth}
       </Typography>
-      <Button variant="contained" onClick={handleLastMonth} sx={{ margin: 2 }}>
-        Previous
-      </Button>
-      <Button variant="contained" onClick={handleNextMonth} sx={{ margin: 2 }}>
+      <Button
+        variant="text"
+        onClick={handleNextMonth}
+        sx={{ margin: 2, color: "green" }}
+      >
         Next
       </Button>
-    </>
+    </Box>
   );
 };
 
