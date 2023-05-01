9107a9c7a0e5c989bcbb9d1662d93a5c08fefb38
Simon Bromfield <simon@simonbromfield>
2023-05-01 20:27:31 +0100

    updated header message

--- Dashboard/src/components/map/FilteredEventsMessage.js
+++ Dashboard/src/components/map/FilteredEventsMessage.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import Link from 'next/link';
 
 const FilteredEventsMessage = ({ count }) => {
   return (
-    <p>
-      <strong>PLEASE NOTE:</strong> {count} events have been filtered from the events list as they have no geo data
-      provided by the protocol integrator, and/or they were created by the &quot;Demo v1&quot; integrator.
-    </p>
+    <>
+      <p>
+        <strong>INFO:</strong> This map displays all the events that have been created by the{' '}
+        <Link href="/integrators">
+          <a className="link">integrators</a>
+        </Link>{' '}
+        of{' '}
+        <a href="https://www.get-protocol.io/" target="_blank" className="link">
+          <strong><em>GET Protocol</em></strong>
+        </a>.
+      </p>
+      <p>
+        <strong>PLEASE NOTE:</strong> {count} events have been filtered from the events list as they have no geo data
+        provided by the protocol integrator, and/or they were created by the &quot;Demo v1&quot; integrator. Learn more about{' '}
+        <a href="https://www.get-protocol.io/" target="_blank" className="link">
+          <strong><em>GET Protocol</em></strong>
+        </a>.
+      </p>
+      <style jsx>{`
+        .link {
+          color: var(--primary-color);
+          text-decoration: none;
+          font-weight: bold;
+          font-style: italic;
+        }
+      `}</style>
+    </>
   );
 };
 
--- Dashboard/src/components/map/Map.js
+++ Dashboard/src/components/map/Map.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
-import { Popup } from 'mapbox-gl';
+import FilteredEventsMessage from "./FilteredEventsMessage";
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltb25icm9tZmllbGQiLCJhIjoiY2t4ZjhpbDlvMGQ1azJ2bnh2dTJzMmVkZiJ9.CtqoMAneppRmRhnj8YDR1A';
 
@@ -12,9 +12,10 @@ const Map = ({ events }) => {
   const getGeoJSON = () => {
     const filteredEvents = events
       .filter(event => (parseFloat(event.longitude) !== 0 || parseFloat(event.latitude) !== 0) && event.integrator.id !== "0");
-
+    console.log('Filtered events:', filteredEvents);
+  
     setFilteredEventsCount(events.length - filteredEvents.length);
-
+  
     const features = filteredEvents.map(event => {
       return {
         type: "Feature",
@@ -27,114 +28,98 @@ const Map = ({ events }) => {
         },
       };
     });
-
+    console.log('Features:', features);
+  
     return {
       type: "FeatureCollection",
       features: features,
     };
   };
-
+      
   useEffect(() => {
     if (!map) {
       const newMap = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/light-v11",
-        center: [-103.5917, 40.6699],
+        center: [4.897070, 52.377956],
         zoom: 3,
         attributionControl: false,
       });
-      newMap.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
+  
       newMap.on("load", () => {
         setMap(newMap);
       });
-
-      // Event listeners for click and mouseenter/mouseleave events
-    newMap.on('click', 'clusters', (e) => {
-      // ... your existing code for handling cluster click
-    });
-
-    newMap.on('click', 'unclustered-point', (e) => {
-      // ... your existing code for handling unclustered-point click
-    });
-
-    newMap.on('mouseenter', 'clusters', () => {
-      newMap.getCanvas().style.cursor = 'pointer';
-    });
-
-    newMap.on('mouseleave', 'clusters', () => {
-      newMap.getCanvas().style.cursor = '';
-    });
-
-    newMap.on('mouseenter', 'unclustered-point', () => {
-      newMap.getCanvas().style.cursor = 'pointer';
-    });
-
-    newMap.on('mouseleave', 'unclustered-point', () => {
-      newMap.getCanvas().style.cursor = '';
-    });
-
+  
     } else {
       setLoading(true);
-
-      map.addSource("events", {
-        type: "geojson",
-        data: getGeoJSON(),
-        cluster: true,
-        clusterMaxZoom: 14,
-        clusterRadius: 50,
-      });
-
-      map.addLayer({
-        id: "clusters",
-        type: "circle",
-        source: "events",
-        filter: ["has", "point_count"],
-        paint: {
-          "circle-color": [
-            "step",
-            ["get", "point_count"],
-            "#51bbd6",
-            100,
-            "#f1f075",
-            750,
-            "#f28cb1",
-          ],
-          "circle-radius": [
-            "step",
-            ["get", "point_count"],
-            20,
-            100,
-            30,
-            750,
-            40,
-          ],
-        },
-      });
-
-      map.addLayer({
-        id: "cluster-count",
-        type: "symbol",
-        source: "events",
-        filter: ["has", "point_count"],
-        layout: {
-          "text-field": "{point_count_abbreviated}",
-          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-          "text-size": 12,
-        },
-      });
-
+  
+      console.log('Adding source and layers...');
+  
+      if (map.getSource("events")) {
+        map.getSource("events").setData(getGeoJSON());
+      } else {
+        map.addSource("events", {
+          type: "geojson",
+          data: getGeoJSON(),
+          cluster: true,
+          clusterMaxZoom: 14,
+          clusterRadius: 50,
+        });
+      }
+  
+      if (!map.getLayer("clusters")) {
+        map.addLayer({
+          id: "clusters",
+          type: "circle",
+          source: "events",
+          filter: ["has", "point_count"],
+          paint: {
+            "circle-color": [
+              "step",
+              ["get", "point_count"],
+              "#51bbd6",
+              100,
+              "#f1f075",
+              750,
+              "#f28cb1",
+            ],
+            "circle-radius": [
+              "step",
+              ["get", "point_count"],
+              20,
+              100,
+              30,
+              750,
+              40,
+            ],
+          },
+        });
+      }
+      
+      if (!map.getLayer("cluster-count")) {
+        map.addLayer({
+          id: "cluster-count",
+          type: "symbol",
+          source: "events",
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12,
+          },
+        });
+      }      
+      
       setLoading(false);
     }
   }, [map, events]);
+  
+           
 
   return (
     <div>
       <div>
-        <p>
-          <strong>PLEASE NOTE:</strong> {filteredEventsCount} events have been filtered from the events list as they have no geo data
-          provided by the protocol integrator, and/or they were created by the &quot;Demo v1&quot; integrator.
-        </p>
+        <FilteredEventsMessage count={filteredEventsCount} />
       </div>
       <div>
         {!map && <div>Generating Map...</div>}
