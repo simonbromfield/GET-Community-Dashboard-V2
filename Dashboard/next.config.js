351c655d35459e50ca103af5d9833f7bd9dc231e
Simon Bromfield <simon@simonbromfield>
2023-05-30 10:18:05 +0100

    trigger maintenance boolean

--- Dashboard/next.config.js
+++ Dashboard/next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
 };
